0f0d6cf852ed6fdcc47630b87fe8cf5c148cb3f6
Fabricio Lukestik <fabri.lkt@gmail.com>
2023-01-10 17:37:29 -0300

    feature(home): Config route to home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+
 function App() {
-  return <div>app</div>
+  return (
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+    </Routes>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App />)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
